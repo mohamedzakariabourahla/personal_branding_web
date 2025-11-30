0e8d2d9941fa22776063ca3a66f4c0e61fd4a954
bourahla mohamed zakaria <mohamedzakariabourahla@gmail.com>
2025-11-30 09:02:29 +0100

    refined worke

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.100.3", "pb.local", "172.20.10.2", "https://pb.local", "https://pb.local:8443"],
+  allowedDevOrigins: ["192.168.100.3", 
+    "pb.local", 
+    "172.20.10.2", 
+    "https://pb.local", 
+    "https://pb.local:8443", 
+    "https://pb.test",
+    "https://pb.test:8443",      
+    "https://192.168.100.3",     
+    "https://192.168.100.3:8443"],
 };
 
 export default nextConfig;
